f8fc2bbfbabeb05112767c33faeaa73e0d28b070
will9191 <willian.fernando040@gmail.com>
2024-07-03 14:36:28 -0300

    Soma de 1 a 10

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Desafios_C_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Sum();
+      
+    }
+
+    static void Sum()
+    {
+        var total = 0;
+        for (int i = 0; i <= 10; i++)
+        {
+            total += i;
+        }
+        Console.WriteLine(total);
+        Console.WriteLine("-----------Fim Soma---------");
+    }
+
+
+}
