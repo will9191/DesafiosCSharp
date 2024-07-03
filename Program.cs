e3303bac01a5c3262ecb4d70172ae6f3a4390aee
will9191 <willian.fernando040@gmail.com>
2024-07-03 14:57:56 -0300

    Calculadora

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ class Program
         Console.WriteLine("Hello, World!");
         Sum();
         IMC();
+        Calculator();
     }
 
     static void Sum()
@@ -75,5 +76,44 @@ class Program
                 break;
         }
         Console.WriteLine("-----------Fim IMC---------");
+    }
+
+    static void Calculator()
+    {
+        int num1 = 0; int num2 = 0;
+
+        Console.WriteLine("Vamos fazer algumas contas!");
+
+        Console.WriteLine("Digite um número!");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite mais um número!");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("Escolha uma opção da lista para a operação desejada!:");
+        Console.WriteLine("\t1 - Somar");
+        Console.WriteLine("\t2 - Subtrair");
+        Console.WriteLine("\t3 - Multiplicar");
+        Console.WriteLine("\t4 - Dividir");
+        Console.Write("Opção escolhida: ");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                Console.WriteLine($"O resultado de: {num1} + {num2} = " + (num1 + num2));
+                break;
+            case "2":
+                Console.WriteLine($"O resultado de: {num1} - {num2} = " + (num1 - num2));
+                break;
+            case "3":
+                Console.WriteLine($"O resultado de:{num1} * {num2} = " + (num1 * num2));
+                break;
+            case "4":
+                Console.WriteLine($"O resultado de: {num1} / {num2} = " + (num1 / num2));
+                break;
+        }
+       
+        Console.WriteLine("-------Fim da operação!-----------");
     }
 }
--- obj/Debug/net8.0/Desafios C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Desafios C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafios C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb58a2e740c2a152509f481a1cac87bc367985f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e25dc378c4ab9ce32958cc1900e196b58c9175a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafios C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafios C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
